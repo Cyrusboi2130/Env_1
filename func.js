f9d3f09796f198d7f12d5fba2b92ac4a0cdffed4
Rehan Sajid <l1f21bsse0457@gmail.com>
2025-01-22 09:44:12 +0500

    Update func.js

--- func.js
+++ func.js
@@ -1,5 +1,5 @@
 document.getElementById('changeColorBtn').addEventListener('click', function() {
-    const colors = ['#FF6347', '#4682B4', '#32CD32', '#FFD700', '#8A2BE2'];
+    const colors = ['#FF6347', '#4682B4', '#32CD32', '#FFD700', '#8A2BE2','#cb231f','#de30ae','#293648','#f4eb49'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.body.style.backgroundColor = randomColor;
 });
